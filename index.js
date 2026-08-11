0122a6cf0964ed5fab0bfe39502c3f3533b7c20a
AliTime <alikmcpacc@gmail.com>
2026-08-11 15:15:14 -0400

    Esqueleto de la pokédex y conexión con la API de pokeapi

---
+++ index.js
@@ -0,0 +1,71 @@
+
+async function search() {
+    loadSpr();
+    loadName();
+    loadID();
+    loadTypes();
+    loadStats();
+}
+
+// Busco el nombre del pokemon con la API
+async function getPokemon(nombre) {
+    try {
+        const URL = `https://pokeapi.co/api/v2/pokemon/${nombre}`;
+        const response = await fetch(URL);
+
+        if (!response.ok){
+            throw new Error(`HTTP ${response.status}: ${nombre} no encontrado`)
+        }
+
+        const data = response.json();
+        console.log(data)
+        return data;
+
+    } catch (error){
+        console.error('Error al obtener pokémon', error.message);
+        throw error;
+    }
+}
+
+//nombre
+function loadName(name) {
+    const nameTag = document.getElementById("name")
+    nameTag.innerHTML = `${name}`;
+    console.log(`${name}`)
+}
+
+//id
+function loadID(id) {
+    const idTag = document.getElementById("identification")
+    idTag.innerHTML = `${id}`;
+    console.log(id);
+}
+
+//tipos
+function loadTypes(name) {
+    const typesTag = document.getElementById("types")
+    typesTag.innerHTML = `${name}`;
+}
+
+//stats
+function loadStats(stats) {
+    const statsTag = document.getElementById("stats")
+    statsTag.innerHTML = `${stats}`;
+}
+
+async function search() {
+    const CONTENT = document.getElementById("pokemonInput").value;
+
+    if (!CONTENT) return;
+
+    try {        
+        const POKEMON = await getPokemon(CONTENT);
+        loadName(POKEMON.name);
+        loadID(POKEMON.id);
+        loadTypes(POKEMON.types);
+    }catch (error){
+        console.error('Error al obtener pokémon', error.message)
+        throw error;
+    }
+
+}
